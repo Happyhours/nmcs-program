686e397ae0b91874375c70dc4b721d6052e54f80
Jonathan Doherty <jonatan.doherty.work@gmail.com>
2014-01-16 19:09:45 +0100

    Fixed syntax errors

--- nmcs/customers/models.py
+++ nmcs/customers/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-def Customer(models.Model):
+
+class Postal(models.Model):
+    postal = models.CharField(max_length=50, primary_key=True)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.postal
+
+        
+class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
@@ -12,20 +21,20 @@ def Customer(models.Model):
     def __str__(self):
         return self.first_name
 
-
-def Telephone(models.Model):
-    number = CharField(max_length=50)
+class Telephone(models.Model):
+    number = models.CharField(max_length=50)
     customer = models.ForeignKey(Customer)
 
     def __str__(self):
         return self.number
 
 
-def Postal(models.Model):
-    postal = models.CharField(max_length=50, primary_key=True)
-    city = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.postal
+
+
+
+
+
+
 
 
