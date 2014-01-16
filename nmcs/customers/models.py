a8449ab0f881294ec8e2ad687a0da3b4d5f89536
Jonathan Doherty <jonatan.doherty.work@gmail.com>
2014-01-16 19:04:35 +0100

    Added Customer, Telephone and Postal models

--- nmcs/customers/models.py
+++ nmcs/customers/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+def Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    street = models.CharField(max_length=100)
+    postal = models.ForeignKey(Postal)
+
+    def __str__(self):
+        return self.first_name
+
+
+def Telephone(models.Model):
+    number = CharField(max_length=50)
+    customer = models.ForeignKey(Customer)
+
+    def __str__(self):
+        return self.number
+
+
+def Postal(models.Model):
+    postal = models.CharField(max_length=50, primary_key=True)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.postal
+
+
