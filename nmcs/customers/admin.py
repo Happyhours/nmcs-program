feb653e9cb3d23b45f23f0d5e5fce2514df7b06d
Jonathan Doherty <jonatan.doherty.work@gmail.com>
2014-01-17 18:17:08 +0100

    Added Mc and Model models to the customers app

--- nmcs/customers/admin.py
+++ nmcs/customers/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Postal, Customer, Telephone
+from .models import Postal, Customer, Telephone, Model, Mc
 # Register your models here.
 
 
 admin.site.register(Postal)
 admin.site.register(Customer)
 admin.site.register(Telephone)
+admin.site.register(Model)
+admin.site.register(Mc)
--- nmcs/customers/models.py
+++ nmcs/customers/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Postal(models.Model):
     postal = models.CharField(max_length=50, primary_key=True)
     city = models.CharField(max_length=50)
@@ -17,10 +16,10 @@ class Customer(models.Model):
     email = models.EmailField()
     street = models.CharField(max_length=100)
     postal = models.ForeignKey(Postal)
-    #objects = CustomerManager() 
 
     def __str__(self):
         return self.first_name
+
 
 class Telephone(models.Model):
     number = models.CharField(max_length=50)
@@ -28,6 +27,32 @@ class Telephone(models.Model):
 
     def __str__(self):
         return self.number
+
+
+class Model(models.Model):
+    model = models.CharField(max_length=50, primary_key=True)
+    brand = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.model
+
+
+class Mc(models.Model):
+    registration_nr = models.CharField(max_length=10, primary_key=True)
+    active = models.BooleanField(default=False)
+    removed = models.BooleanField(default=False)
+    year = models.CharField(max_length=20)
+    motor = models.CharField(max_length=50)
+    km = models.CharField(max_length=20)
+    model = models.ForeignKey(Model)
+    customer = models.ForeignKey(Customer)
+
+    def __str__(self):
+        return self.registration_nr
+
+
+
+
 
 
 
