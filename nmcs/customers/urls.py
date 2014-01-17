dfd18f8818f38596bb20b6a10cd5470f55f4f5ec
Jonathan Doherty <jonatan.doherty.work@gmail.com>
2014-01-17 17:28:51 +0100

    Added CustomerListView and search feature

--- nmcs/customers/urls.py
+++ nmcs/customers/urls.py
@@ -3,6 +3,19 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<pk>\d+)$', views.CustomerDetailView.as_view(), name='customer-detail')
+    url(
+        r'^(?P<pk>\d+)$',
+        views.CustomerDetailView.as_view(), 
+        name='customer-detail'
+
+    ),
+    url(
+        r"^$",
+        views.CustomerListView.as_view(),
+        name='customer-list'
+
+    ),
+
+
 
 )
--- nmcs/customers/views.py
+++ nmcs/customers/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
 from django.views import generic
+from django.db.models import Q
 
 # Create your views here.
-from .models import Postal, Customer, Telephone
+from .models import Customer
+
+
 
 class CustomerDetailView(generic.DetailView):
     model = Customer
-    context_object_name = 'customer'
+    context_object_name = 'customer_detail'
     template_name = 'customers/customer_detail.html'
 
 
-#def index(request):
-#    return HttpResponse("Hello world you are at index page")
+class CustomerListView(generic.ListView):
+    model = Customer
+    context_object_name = 'customer_list'
+    template_name = 'customers/customer_list.html'
+
+    def get_queryset(self):
+        # Fetch queryset from the parent(ListView) get_queryset.
+        queryset = super(CustomerListView, self).get_queryset()
+
+        # Get the q GET parameter
+        q = self.request.GET.get("q")
+        if q:
+            # Return a filtered queryset
+            return queryset.filter(
+                Q(first_name__icontains=q) |
+                Q(last_name__icontains=q) |
+                Q(email__icontains=q)
+            )
+
+        else:
+            queryset = []
+            return queryset
